b207e28094bde980d49bfc154a16bc22f5a0b7a0
junsugi <junesuk96@gmail.com>
2020-05-06 21:46:16 +0900

    add get request

--- src/main/java/com/mju/petcares/controller/UserController.java
+++ src/main/java/com/mju/petcares/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.mju.petcares.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.annotation.JsonView;
 import com.mju.petcares.dto.UserDTO;
 
 
@@ -28,4 +31,14 @@ public class UserController {
 		return "home";
 	}
 	
+	//@JsonView(UserDTO.class)
+	@RequestMapping(value = "/main", method = RequestMethod.GET)
+	public UserDTO main() {
+		System.out.println("---------들어왔당---------");
+		UserDTO user = new UserDTO();
+		user.setUserId("test");
+		user.setPassword("12345");
+		
+		return user;
+	}
 }
